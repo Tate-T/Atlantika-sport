e7c78d898c408b99eb217d8823c418be9b47e555
ZACRAN1C <162379113+ZACRAN1C@users.noreply.github.com>
2025-01-09 23:38:33 +0200

    add modal

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-open]"),
+      closeModalBtn: document.querySelector("[data-close]"),
+      modal: document.querySelector("[data-modal]"),
+      backdrop: document.querySelector(".backdrop"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.backdrop.addEventListener("click", toggleModal);
+  function toggleModal(e) {
+      if (e.currentTarget === refs.backdrop && e.target !== e.currentTarget)
+        return;
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
